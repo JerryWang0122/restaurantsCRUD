1101da9dd8368d5042dca18dd77b6fae7998eb87
JerryWang0122 <school832@gmail.com>
2023-12-14 21:40:43 +0800

    feat: implement register

--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+
+const db = require('../models')
+const User = db.User
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -14,7 +18,38 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('post register data')
+  const { name, email, password, confirmPassword} = req.body
+
+  if (!email || !password) {
+    req.flash('error', 'email 和 password 為必填')
+    res.redirect('back')
+  }
+  if (password !== confirmPassword) {
+    req.flash('error', '驗證密碼與密碼不符')
+    res.redirect('back')
+  }
+
+  return User.count({ where: { email }})
+    .then(amount => {
+      if (amount > 0) {
+        req.flash('error', 'email 已註冊')
+        return
+      }
+      return bcrypt.hash(password, 10)
+        .then(hash => User.create({ name, email, password: hash }))
+    })
+    .then(user => {
+      if (!user) {
+        return res.redirect('back')
+      }
+
+      req.flash('success', '註冊成功')
+      return res.redirect('/login')
+    })
+    .catch(error => {
+      error.errorMessage = '發生錯誤，註冊失敗'
+      next(error)
+    })
 })
 
 router.post('/logout', (req, res) => {
